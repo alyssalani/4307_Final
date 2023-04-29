1633d142c154894227e29d08d2c277cd4c034bb6
sneezinatchew <benjaminjenks02@gmail.com>
2023-04-28 18:24:42 -0600

    income table

---
+++ populate_income.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sqlite3
+import csv
+connection = sqlite3.connect("Music.db")
+cursor = connection.cursor()
+
+def populate_table(csv_file, table_name):
+    rows = []
+    with open(csv_file, 'r') as f:
+        csvreader = csv.reader(f)
+        header = next(csvreader)
+        for i in csvreader:
+            rows.append(i)
+    print(rows)
+    for x in rows:
+        yearly_income = x[0]
+        status = x[1]
+        cursor.execute("INSERT INTO " + table_name + " (income,status) VALUES('{}', '{}')".format(yearly_income, status))
+        connection.commit()
+        
+    
+populate_table("income.csv", "Income")
