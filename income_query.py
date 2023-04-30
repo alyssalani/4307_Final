8bd563ffb2152554c90eb47522afe7559a053854
sneezinatchew <benjaminjenks02@gmail.com>
2023-04-30 11:58:25 -0600

    ben queries

---
+++ income_query.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sqlite3
+connection = sqlite3.connect("Music.db")
+cursor = connection.cursor()
+
+
+# first query, richest people and what genre of music is their favorite
+rows = cursor.execute("SELECT first_name, last_name, genre, state, income FROM Person, Income
+WHERE Person.personID = Income.personID
+ORDER BY income DESC").fetchall()
+print(rows)
+
+#second query, people in the same state with  similar taste in music
+rows2 = cursor.execute("SELECT first_name, last_name, state, genre
+        FROM Person
+        GROUP BY state
+        ORDER BY genre").fetchall()
+print(rows2)
+
