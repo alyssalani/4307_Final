b355e29b6e1f547f624c849c03a737ca261c2e7e
alyssalani <89876757+alyssalani@users.noreply.github.com>
2023-04-29 18:27:42 -0600

    Add populate songs

--- populate_everything.py
+++ populate_everything.py
@@ -1,10 +1,12 @@
 # populate_everything.py
 
-import populate_income, populate_people
+import populate_income, populate_people, populate_songs
 
 def populate_everything():
     populate_people
     populate_income
+    populate_songs
+    
     print('Populate everything done')
 
 populate_everything()
