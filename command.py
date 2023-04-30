c682ecf060ece55d77e881aa90d9d96852f32b59
alyssalani <89876757+alyssalani@users.noreply.github.com>
2023-04-30 14:27:30 -0600

    Just missing 1 of Alyssa's queries

--- command.py
+++ command.py
@@ -9,11 +9,41 @@ cursor = connection.cursor()
 match command: 
   
   case 'richest':
-    
+    rows = cursor.execute("""SELECT first_name, last_name, genre, state, income FROM Person, Income
+    WHERE Person.personID = Income.personID
+    ORDER BY income DESC""").fetchall()
+    for i in rows:
+      print(i)
+
   case 'nearby':
-    
+    rows2 = cursor.execute("""SELECT first_name, last_name, state, genre
+        FROM Person
+        ORDER BY state, genre
+        """).fetchall()
+    for j in rows2:
+      print(j)
   case 'recommended':
-    
+    id = sys.argv[1]
+    cursor.execute("""
+    SELECT song_name, artistName, Song.genre FROM Song, Person 
+    WHERE Song.genre = Person.genre AND Person.personID = ?""", id)
+
+    userList = cursor.fetchall()
+
+    for x in userList:
+        print(x)
+        
   case 'platforms':
     
   case 'popularGenre':
+    cursor.execute("""
+        SELECT genre, COUNT(genre) AS count
+        FROM PERSON
+        GROUP BY genre
+        ORDER BY count DESC
+        """)
+    userList = cursor.fetchall()
+    for x in userList:
+        name = str(x[0])
+        genre = str(x[1])
+        print(name + ": " + genre)
